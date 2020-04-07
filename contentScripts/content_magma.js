1811010ba0b2c3b927a1d083db1a2610d53035ec
Daniel Kluge <daniel.kluge.98@gmx.de>
2020-04-07 14:54:32 +0200

    Removed jquerry dependency, works on http too (for the unsecure guys)

--- contentScripts/content_magma.js
+++ contentScripts/content_magma.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if($('button[name="loginButton"]') && $('select[name="wayfSelect"]')){
-        console.log('Found')
-        chrome.storage.local.get(['asdf', 'fdsa'], function(result) {
+document.addEventListener("DOMContentLoaded", function() {
+    if(document.getElementsByName("loginButton") && document.getElementsByName("wayfSelect")){
+        console.log("Found")
+        chrome.storage.local.get(["asdf", "fdsa"], function(result) {
             if (!(result.asdf === undefined  || result.fdsa === undefined)) { 
                 chrome.runtime.sendMessage({cmd: "show_ok_badge", timeout: 2000})
                 chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
                 document.getElementsByName("wayfSelect")[0].value = "11"
-                $('button[name="loginButton"]').click()
+                document.getElementsByName("loginButton")[0].click()
             } else {
               
             }
         });
     }
 })
-console.log('Auto Login to magma.')
+console.log("Auto Login to magma.")
