e5da5d2482b9fe99f3819c368f643878e28efc32
oliver <oliver@oliver.hp>
2020-04-27 20:38:19 +0200

    added qis login bug

--- contentScripts/content_idp2_1.js
+++ contentScripts/content_idp2_1.js
@@ -1,37 +1,39 @@
 chrome.storage.local.get(['isEnabled'], function(result) {
-  console.log('-1')
   if(result.isEnabled) {  
-    console.log('0')
-    document.addEventListener('DOMContentLoaded', function() {  
-      if (document.getElementById("username")) {
-        console.log('1')
-        chrome.runtime.sendMessage({cmd: 'get_user_data'}, function(result) {
-          if (!(result.fdsa === undefined || result.asdf === undefined)) {
-            console.log('2')
-            chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
-            document.getElementById('username').value = (result.asdf)
-            document.getElementById('password').value = (result.fdsa)
-            document.getElementsByName("_eventId_proceed")[0].click()
-          } else {
-            chrome.runtime.sendMessage({cmd: "no_login_data"});
-          }
-        });
-      } else if (document.getElementsByName("_eventId_proceed")[0]){
-            console.log('3')
-            document.getElementsByName("_eventId_proceed")[0].click()
-          chrome.runtime.sendMessage({cmd: "perform_login"})    
-          chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
-      } else if (document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0]) {
-        if(document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0].innerHTML === "TUD - TU Dresden - Single Sign On - Veraltete Anfrage" ||
-           document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0].innerHTML === "TUD - TU Dresden - Single Sign On - Stale Request"){
-          window.location.replace("https://bildungsportal.sachsen.de/opal/login")
-        }
-      }
-    })
+    if (document.readyState !== 'loading') {
+      logInQis()
+    } else {
+      document.addEventListener('DOMContentLoaded', function() {  
+      logInQis()
+      })
+    }
     console.log('Auto Login to TU Dresden Auth.')
   }
 })
-console.log('-2')
+
+function logInQis() {
+  if (document.getElementById("username")) {
+    chrome.runtime.sendMessage({cmd: 'get_user_data'}, function(result) {
+      if (!(result.fdsa === undefined || result.asdf === undefined)) {
+        chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
+        document.getElementById('username').value = (result.asdf)
+        document.getElementById('password').value = (result.fdsa)
+        document.getElementsByName("_eventId_proceed")[0].click()
+      } else {
+        chrome.runtime.sendMessage({cmd: "no_login_data"});
+      }
+    });
+  } else if (document.getElementsByName("_eventId_proceed")[0]){
+      document.getElementsByName("_eventId_proceed")[0].click()
+      chrome.runtime.sendMessage({cmd: "perform_login"})    
+      chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
+  } else if (document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0]) {
+    if(document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0].innerHTML === "TUD - TU Dresden - Single Sign On - Veraltete Anfrage" ||
+       document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0].innerHTML === "TUD - TU Dresden - Single Sign On - Stale Request"){
+      window.location.replace("https://bildungsportal.sachsen.de/opal/login")
+    }
+  }
+}
 
 
 
