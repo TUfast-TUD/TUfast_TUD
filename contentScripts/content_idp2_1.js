f0ebca5a2f3cbf7613c5322034ccc2872335676e
oliver <oliver@oliver.hp>
2020-04-27 20:15:13 +0200

    added english version

--- contentScripts/content_idp2_1.js
+++ contentScripts/content_idp2_1.js
@@ -1,9 +1,13 @@
 chrome.storage.local.get(['isEnabled'], function(result) {
+  console.log('-1')
   if(result.isEnabled) {  
+    console.log('0')
     document.addEventListener('DOMContentLoaded', function() {  
       if (document.getElementById("username")) {
+        console.log('1')
         chrome.runtime.sendMessage({cmd: 'get_user_data'}, function(result) {
           if (!(result.fdsa === undefined || result.asdf === undefined)) {
+            console.log('2')
             chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
             document.getElementById('username').value = (result.asdf)
             document.getElementById('password').value = (result.fdsa)
@@ -13,11 +17,13 @@ chrome.storage.local.get(['isEnabled'], function(result) {
           }
         });
       } else if (document.getElementsByName("_eventId_proceed")[0]){
-          document.getElementsByName("_eventId_proceed")[0].click()
+            console.log('3')
+            document.getElementsByName("_eventId_proceed")[0].click()
           chrome.runtime.sendMessage({cmd: "perform_login"})    
           chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
       } else if (document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0]) {
-        if(document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0].innerHTML === "TUD - TU Dresden - Single Sign On - Veraltete Anfrage"){
+        if(document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0].innerHTML === "TUD - TU Dresden - Single Sign On - Veraltete Anfrage" ||
+           document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0].innerHTML === "TUD - TU Dresden - Single Sign On - Stale Request"){
           window.location.replace("https://bildungsportal.sachsen.de/opal/login")
         }
       }
@@ -25,6 +31,8 @@ chrome.storage.local.get(['isEnabled'], function(result) {
     console.log('Auto Login to TU Dresden Auth.')
   }
 })
+console.log('-2')
+
 
 
 
