36e09503738245aaf794ca00e1f649ea56c258d0
oliver <oliver@oliver.hp>
2020-05-01 21:19:46 +0200

    fixed typo

--- contentScripts/content_fwd_jexam.js
+++ contentScripts/content_fwd_jexam.js
@@ -1,5 +1,5 @@
 chrome.storage.local.get(['fwdEnabled'], function(result) {
-    if(esult.fwdEnabled) {
+    if(result.fwdEnabled) {
         console.log("fwd to jexam")
         chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})    
         window.location.replace("https://jexam.inf.tu-dresden.de/de.jexam.web.v4.5/spring/welcome")
