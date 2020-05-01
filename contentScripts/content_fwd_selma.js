a100153d4cd0df6ea1f372f71371cdbdc67fb197
oliver <oliver@oliver.hp>
2020-05-01 21:32:50 +0200

    improved forwarding

--- contentScripts/content_fwd_selma.js
+++ contentScripts/content_fwd_selma.js
@@ -2,6 +2,6 @@ chrome.storage.local.get(['fwdEnabled'], function(result) {
     if(result.fwdEnabled) {
         console.log('fwd to selma')
         chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
-        window.location.replace("https://selma.tu-dresden.de/APP/EXTERNALPAGES/-N000000000000001,-N000155,-AEXT_willkommen")
+        window.location.replace("https://selma.tu-dresden.de/APP/MLSSTART/-N982027801258054,-N000090,")
     }
 })
