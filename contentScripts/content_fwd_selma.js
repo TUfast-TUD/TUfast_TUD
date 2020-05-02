1dd62b8e94f02f7b8569bf5681a0902bc6e4ada2
oliver <oliver@oliver.hp>
2020-05-02 11:22:13 +0200

    added code comment

--- contentScripts/content_fwd_selma.js
+++ contentScripts/content_fwd_selma.js
@@ -2,8 +2,7 @@ chrome.storage.local.get(['fwdEnabled'], function(result) {
     if(result.fwdEnabled) {
         console.log('fwd to selma')
         chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
-        //each user has a unique url in selma
-        //it would be nice to implement forwarding to this url.
+        //There is a unique session url. That is why user always has to be fwd to login page :/
         window.location.replace("https://selma.tu-dresden.de/APP/EXTERNALPAGES/-N000000000000001,-N000155,-AEXT_willkommen")
     }
 })
