be73f429705934f19b23a91d540ee28e17e8976e
OliEfr <ollihaus@t-online.de>
2020-11-02 18:07:27 +0100

    open new tab, but still downloading

--- contentScripts/content_bildungsportal_viewPDFinBrowser.js
+++ contentScripts/content_bildungsportal_viewPDFinBrowser.js
@@ -1,5 +1,5 @@
 chrome.storage.local.get(['PDFViewerIsEnabled'], function (result) {
-    if (result.PDFViewerIsEnabled) {
+    //if (result.PDFViewerIsEnabled) {
         //on load
         document.addEventListener("DOMNodeInserted", function (e) {
            modifyPdfLinks()
@@ -8,17 +8,17 @@ chrome.storage.local.get(['PDFViewerIsEnabled'], function (result) {
         window.addEventListener("load", function () {
             modifyPdfLinks()
         }, true)
-    }
+    //}
 })
 
 function modifyPdfLinks() {
-//Problem: cannot open link in new tab :/
-    /*let links = document.getElementsByTagName("a")
-for (let idx=0;idx<links.length;idx++){
-  if(links[idx].href.includes(".pdf")){
-    links[idx].setAttribute("target", "_new")
-}
-}*/
+    //Modify js so that link is opened in new tab
+    let links = document.getElementsByTagName("a")
+    for (let idx=0;idx<links.length;idx++){
+        if(links[idx].href.includes(".pdf")){
+            links[idx].setAttribute("onclick", "window.open(this.href, '_blank'); return false;")
+        }
+    }
     
 }
 
