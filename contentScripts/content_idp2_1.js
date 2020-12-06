9a5b63c9f9bac29f514b5182e3ec0f4b588ac7f0
OliEfr <ollihaus@t-online.de>
2020-12-06 15:37:45 +0100

    forward idp page also if auto-login not enabled

--- contentScripts/content_idp2_1.js
+++ contentScripts/content_idp2_1.js
@@ -1,28 +1,28 @@
 chrome.storage.local.get(['isEnabled'], function(result) {
-  if(result.isEnabled) {  
     if (document.readyState !== 'loading') {
-      logInQis()
+      logInQis(result.isEnabled)
     } else {
       document.addEventListener('DOMContentLoaded', function() {  
-      logInQis()
+        logInQis(result.isEnabled)
       })
-    }
     console.log('Auto Login to TU Dresden Auth.')
-  }
+  } 
 })
 
-function logInQis() {
+function logInQis(enabled) {
   if (document.getElementById("username")) {
-    chrome.runtime.sendMessage({cmd: 'get_user_data'}, function(result) {
-      if (!(result.fdsa === undefined || result.asdf === undefined)) {
-        chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
-        document.getElementById('username').value = (result.asdf)
-        document.getElementById('password').value = (result.fdsa)
-        document.getElementsByName("_eventId_proceed")[0].click()
-      } else {
-        chrome.runtime.sendMessage({cmd: "no_login_data"});
-      }
-    });
+    if(enabled) { //yes, this if-statements need to be nested!
+      chrome.runtime.sendMessage({cmd: 'get_user_data'}, function(result) {
+        if (!(result.fdsa === undefined || result.asdf === undefined)) {
+          chrome.runtime.sendMessage({cmd: "save_clicks", click_count: 1})
+          document.getElementById('username').value = (result.asdf)
+          document.getElementById('password').value = (result.fdsa)
+          document.getElementsByName("_eventId_proceed")[0].click()
+        } else {
+          chrome.runtime.sendMessage({cmd: "no_login_data"});
+        }
+      })
+    }
   } else if (document.getElementsByName("_eventId_proceed")[0]){
       document.getElementsByName("_eventId_proceed")[0].click()
       chrome.runtime.sendMessage({cmd: "perform_login"})    
@@ -30,7 +30,7 @@ function logInQis() {
   } else if (document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0]) {
     if(document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0].innerHTML === "TUD - TU Dresden - Single Sign On - Veraltete Anfrage" ||
        document.querySelectorAll('body > div:nth-child(2) > div:nth-child(1) > b')[0].innerHTML === "TUD - TU Dresden - Single Sign On - Stale Request"){
-      window.location.replace("https://bildungsportal.sachsen.de/opal/login")
+        window.location.replace("https://bildungsportal.sachsen.de/opal/login")
     }
   }
 }
