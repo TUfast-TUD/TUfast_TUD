01ae46357c4e64083e379de1ff9ec4a972034c5c
OliEfr <ollihaus@t-online.de>
2021-01-16 13:48:26 +0100

    fix settings.js

--- settings.js
+++ settings.js
@@ -72,14 +72,14 @@ function displayEnabled() {
     document.getElementById("additionalNotification").checked = result.additionalNotificationOnNewMail
   })
 
-  chrome.storage.local.get(['pdfInInline'], function(result) {
+  chrome.storage.local.get(['pdfInInline'], function (result) {
     this.document.getElementById('switch_pdf_inline').checked = result.pdfInInline
-    if(!result.pdfInInline){
+    if (!result.pdfInInline) {
       document.getElementById("switch_pdf_newtab_block").style.visibility = "hidden";
     }
   })
 
-  chrome.storage.local.get(['pdfInNewTab'], function(result) {
+  chrome.storage.local.get(['pdfInNewTab'], function (result) {
     this.document.getElementById('switch_pdf_newtab').checked = result.pdfInNewTab
   })
   /*chrome.storage.local.get(['dashboardDisplay'], function(result) {
@@ -246,58 +246,39 @@ window.onload = async function () {
     }
   });
 
-    document.getElementById("switch_pdf_inline").addEventListener("click", function () {
-            chrome.storage.local.set({ pdfInInline: this.checked }, function () {});
-            document.getElementById("switch_pdf_newtab_block").style.visibility = this.checked ? "visible" : "hidden";
+  document.getElementById("switch_pdf_inline").addEventListener("click", function () {
+    chrome.storage.local.set({ pdfInInline: this.checked }, function () { });
+    document.getElementById("switch_pdf_newtab_block").style.visibility = this.checked ? "visible" : "hidden";
 
-            if (this.checked) {
-              //request necessary permissions
-              chrome.permissions.request( { permissions: [ "webRequest", "webRequestBlocking" ], origins: [ "https://bildungsportal.sachsen.de/opal/*" ] },
-                function (granted) {
-                  if (granted) {
-                    chrome.runtime.sendMessage({ cmd: "toggle_pdf_inline_setting", enabled: true });
-                  } else {
-                    //permission granting failed :( -> revert checkbox settings
-                    chrome.storage.local.set({ pdfInInline: false });
-                    this.document.getElementById("switch_pdf_inline").checked = false;
-                    alert("TUfast braucht diese Berechtigung, um die PDFs im Browser anzeigen zu koennen");
-                  }
-                }
-              );
-            } else {
-              //disable "pdf in new tab" setting since it doesn't make any sense without inline pdf
-              chrome.storage.local.set({ pdfInNewTab: false });
-              document.getElementById("switch_pdf_newtab").checked = false;
-              chrome.runtime.sendMessage({ cmd: "toggle_pdf_inline_setting", enabled: false });
-            }
-        });
+    if (this.checked) {
+      //request necessary permissions
+      chrome.permissions.request({ permissions: ["webRequest", "webRequestBlocking"], origins: ["https://bildungsportal.sachsen.de/opal/*"] },
+        function (granted) {
+          if (granted) {
+            chrome.runtime.sendMessage({ cmd: "toggle_pdf_inline_setting", enabled: true });
+          } else {
+            //permission granting failed :( -> revert checkbox settings
+            chrome.storage.local.set({ pdfInInline: false });
+            this.document.getElementById("switch_pdf_inline").checked = false;
+            alert("TUfast braucht diese Berechtigung, um die PDFs im Browser anzeigen zu koennen. Versuche es erneut.");
+          }
+        }
+      );
+    } else {
+      //disable "pdf in new tab" setting since it doesn't make any sense without inline pdf
+      chrome.storage.local.set({ pdfInNewTab: false });
+      document.getElementById("switch_pdf_newtab").checked = false;
+      chrome.runtime.sendMessage({ cmd: "toggle_pdf_inline_setting", enabled: false });
+    }
+  });
 
-    document.getElementById("switch_pdf_newtab").addEventListener("click", function () {
-          chrome.storage.local.set({ pdfInNewTab: this.checked }, function () {});
-        });
+  document.getElementById("switch_pdf_newtab").addEventListener("click", function () {
+    chrome.storage.local.set({ pdfInNewTab: this.checked }, function () { });
+  });
 
-    //set all switches and elements
-    displayEnabled()
+  //set all switches and elements
+  displayEnabled()
 
-    //get things from storage
-    chrome.storage.local.get(['saved_click_counter', "openSettingsPageParam", "isEnabled"], (result) => {
-      //set text on isEnabled
-      if(result.isEnabled) {
-        document.getElementById('status_msg').innerHTML = "<font color='green'>Du bist angemeldet und wirst automatisch in Opal & Co. eingeloggt.</font>"
-      } 
-      //else {
-      //  document.getElementById('status_msg').innerHTML = "<font color='grey'>Du bist nicht angemeldet.</font>"
-      //}
-      //update saved clicks  
-      //see if any params are available
-      if(result.openSettingsPageParam === "auto_login_settings"){ setTimeout(function(){ this.document.getElementById("auto_login_settings").click(); }, 200);}
-      else if(result.openSettingsPageParam === "time_settings"){ setTimeout(function(){ this.document.getElementById("time_settings").click(); }, 200);}
-      /*else {document.getElementsByTagName("button")[0].click()}*/
-    
-      if (result.saved_click_counter === undefined) {result.saved_click_counter = 0}
-      this.document.getElementById("settings_comment").innerHTML="Bereits " + clicksToTimeNoIcon(result.saved_click_counter)
-      this.document.getElementById("time_settings").innerHTML = clicksToTime(result.saved_click_counter)
-      chrome.storage.local.set({openSettingsPageParam: false}, function() {})
 
   //get things from storage
   chrome.storage.local.get(['saved_click_counter', "openSettingsPageParam", "isEnabled", "gotInteractionOnHostPermissionExtension1"], (result) => {
