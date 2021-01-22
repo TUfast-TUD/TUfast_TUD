12bee9abd5b2c26a304ca9e17f731cd8595d8b6e
OliEfr <ollihaus@t-online.de>
2021-01-22 11:22:04 +0100

    show share-page in popup

--- popup.js
+++ popup.js
@@ -67,10 +67,14 @@ function openSettings() {
 }
 
 function openShare() {
+    /*
     chrome.runtime.sendMessage({ cmd: 'open_share_page', param: "" }, function (result) { }) //for some reason I need to pass empty param - else it wont work in ff
     let isFirefox = navigator.userAgent.includes("Firefox/")  //attention: no failsave browser detection
     if (isFirefox) window.close()
     return false //Required for ff
+    */
+    document.getElementById("list").innerHTML = '<object type="text/html" style="height: 450px; width: 510px; overflow: hidden;" data="share.html" ></object>'
+
 }
 
 function openSettingsTimeSection() {
