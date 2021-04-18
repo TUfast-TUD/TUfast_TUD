e6adabd9a3747d3d78a372ce925823c2a12b35c8
Lennart Guderian <lennart@guderian.de>
2021-04-18 12:52:07 +0200

    added color info

--- freshContent/gradeTable.js
+++ freshContent/gradeTable.js
@@ -2,6 +2,17 @@ const table_html = '<div id="table"> \
 <vs-table hover-flat :data="table"> \
   <template slot="header"> \
     <h1>Deine Notenübersicht</h1> \
+    <div class="info-row"> \
+	<div class="info-row__info"> \
+		<div class="square blue"></div><h5>Module</h5> \
+	</div> \
+	<div class="info-row__info"> \
+		<div class="square green"></div><h5>Bestandene Prüfung</h5> \
+	</div> \
+	<div class="info-row__info"> \
+		<div class="square red"></div><h5>Nicht bestandene Prüfung</h5> \
+	</div> \
+</div> \
   </template> \
   <template slot="thead"> \
     <vs-th v-for="(header_text, index) in table[1]"  :sort-key="`${index}`" :key="index"> {{header_text}} </vs-th> \
