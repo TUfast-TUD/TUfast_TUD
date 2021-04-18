2a9d81fd20af930e7af2397e4b1ca92870abb547
OliEfr <ollihaus@t-online.de>
2021-04-18 20:10:23 +0200

    formatting; this commit is just a test

--- settings.js
+++ settings.js
@@ -6,25 +6,25 @@ function saveUserData() {
     return false
   } else {
     chrome.storage.local.set({ isEnabled: true }, function () { }) //need to activate auto login feature
-    chrome.runtime.sendMessage({ cmd: "clear_badge" });
+    chrome.runtime.sendMessage({ cmd: "clear_badge" })
     chrome.runtime.sendMessage({ cmd: "set_user_data", userData: { asdf: asdf, fdsa: fdsa } })
     document.getElementById('status_msg').innerHTML = ""
     document.getElementById("save_data").innerHTML = '<font>Gespeichert!</font>'
-    document.getElementById("save_data").disabled = true;
+    document.getElementById("save_data").disabled = true
     document.getElementById("save_data").style.backgroundColor = "rgb(47, 143, 18)"
     document.getElementById("username_field").value = ""
     document.getElementById("password_field").value = ""
     document.getElementById('status_msg').innerHTML = "<font color='green'>Du bist angemeldet und wirst automatisch in Opal & Co. eingeloggt.</font>"
     setTimeout(() => {
       document.getElementById("save_data").innerHTML = 'Speichern'
-      document.getElementById("save_data").disabled = false;
+      document.getElementById("save_data").disabled = false
     }, 2000)
   }
 }
 
 
 function deleteUserData() {
-  chrome.runtime.sendMessage({ cmd: "clear_badge" });
+  chrome.runtime.sendMessage({ cmd: "clear_badge" })
   chrome.storage.local.set({ Data: "undefined" }, function () { }) //this is how to delete user data!
   chrome.storage.local.set({ isEnabled: false }, function () { }) //need to deactivate auto login feature
   // -- also delete courses in dashboard
@@ -46,7 +46,7 @@ function deleteUserData() {
   document.getElementById("password_field").value = ""
   document.getElementById('status_msg').innerHTML = "<font color='grey'>Du bist nicht angemeldet.</font>"
   setTimeout(() => {
-    document.getElementById("delete_data").innerHTML = 'Alle Daten l&ouml;schen';
+    document.getElementById("delete_data").innerHTML = 'Alle Daten l&ouml;schen'
     document.getElementById("delete_data").style.backgroundColor = "grey"
     document.getElementById("delete_data").disabled = false
     chrome.storage.local.get(['Data'], function (result) {
@@ -82,7 +82,7 @@ function displayEnabled() {
   chrome.storage.local.get(['pdfInInline'], function (result) {
     this.document.getElementById('switch_pdf_inline').checked = result.pdfInInline
     if (!result.pdfInInline) {
-      document.getElementById("switch_pdf_newtab_block").style.visibility = "hidden";
+      document.getElementById("switch_pdf_newtab_block").style.visibility = "hidden"
     }
   })
 
@@ -146,7 +146,7 @@ async function toggleOWAfetch() {
       alert("TUfast braucht diese Berechtigung, um regelmaessig alle Mails abzurufen. Bitte druecke auf 'Erlauben'.")
       return
     }
-  });
+  })
   //    }
   //  } 
   //);
@@ -200,7 +200,7 @@ function requestHostPermissionS() {
       alert("Okay, das ist schade. Trotzdem wird TUfast bei dir weiterhin Funktionieren. Entdecke auf dieser Seite jetzt alle Funktionen von TUfast!")
       document.getElementById("addition_host_permissions").remove()
     }
-  });
+  })
 }
 
 async function getAvailableRockets() {
@@ -309,7 +309,7 @@ function rocketIconSelectionChange() {
   chrome.storage.local.set({ selectedRocketIcon: '{"id": "' + rocketID + '", "link": "' + imgSrc + '"}' }, function () { })
   chrome.browserAction.setIcon({
     path: imgSrc
-  });
+  })
 }
 
 async function enableRocketIcon() {
@@ -329,14 +329,14 @@ async function enableRocketIcon() {
   await new Promise(r => setTimeout(r, 7000))
 
   //change picture and text and enable radio button
-  let timestamp = new Date().getTime();
+  let timestamp = new Date().getTime()
   let rocketNode = document.querySelectorAll("#" + rocketID)[0]
   let rocketImage = rocketNode.parentElement.querySelectorAll("img")[0]
   let rocketText = rocketNode.parentElement.querySelectorAll("text")[0]
   let rocketInput = rocketNode.parentElement.querySelectorAll("input")[0]
-  rocketImage.src = rocketIconsConfig[rocketID]["IconPathEnabled"] + "?t =" + timestamp;
+  rocketImage.src = rocketIconsConfig[rocketID]["IconPathEnabled"] + "?t =" + timestamp
   rocketText.innerHTML = rocketIconsConfig[rocketID]["innerHTMLEnabled"]
-  rocketInput.removeAttribute("disabled");
+  rocketInput.removeAttribute("disabled")
 
 }
 
@@ -361,7 +361,7 @@ window.onload = async function () {
   if (isFirefox) { document.getElementById("additionNotificationSection").style.display = "none" }
 
   //add additional notification checkbox listener
-  var checkbox = document.getElementById("additionalNotification");
+  var checkbox = document.getElementById("additionalNotification")
   checkbox.addEventListener('change', function () {
     if (this.checked) {
       //only if owa fetch enables
@@ -376,7 +376,7 @@ window.onload = async function () {
     } else {
       chrome.storage.local.set({ additionalNotificationOnNewMail: false })
     }
-  });
+  })
 
   //add storage listener for autologin
   chrome.storage.onChanged.addListener(function (changes, namespace) {
@@ -389,43 +389,43 @@ window.onload = async function () {
         document.getElementById("settings_comment").innerHTML = "<strong>F&uuml;r dieses Feature gib hier deine Zugangsdaten ein.</strong>"
       }
     }
-  });
+  })
 
   document.getElementById("switch_pdf_inline").addEventListener("click", function () {
-    chrome.storage.local.set({ pdfInInline: this.checked }, function () { });
-    document.getElementById("switch_pdf_newtab_block").style.visibility = this.checked ? "visible" : "hidden";
+    chrome.storage.local.set({ pdfInInline: this.checked }, function () { })
+    document.getElementById("switch_pdf_newtab_block").style.visibility = this.checked ? "visible" : "hidden"
 
     if (this.checked) {
       //request necessary permissions
       chrome.permissions.request({ permissions: ["webRequest", "webRequestBlocking"], origins: ["https://bildungsportal.sachsen.de/opal/*"] },
         function (granted) {
           if (granted) {
-            chrome.runtime.sendMessage({ cmd: "toggle_pdf_inline_setting", enabled: true });
-            if(isFirefox){
+            chrome.runtime.sendMessage({ cmd: "toggle_pdf_inline_setting", enabled: true })
+            if (isFirefox) {
               alert("Perfekt! Bitte starte den Browser einmal neu, damit die Einstellungen uebernommen werden!")
               chrome.storage.local.set({ openSettingsPageParam: "opalCustomize", openSettingsOnReload: true }, function () { })
               chrome.runtime.sendMessage({ cmd: 'reload_extension' }, function (result) { })
             }
           } else {
             //permission granting failed :( -> revert checkbox settings
-            chrome.storage.local.set({ pdfInInline: false });
-            this.document.getElementById("switch_pdf_inline").checked = false;
-            alert("TUfast braucht diese Berechtigung, um die PDFs im Browser anzeigen zu koennen. Versuche es erneut.");
-            document.getElementById("switch_pdf_newtab_block").style.visibility = "hidden";
+            chrome.storage.local.set({ pdfInInline: false })
+            this.document.getElementById("switch_pdf_inline").checked = false
+            alert("TUfast braucht diese Berechtigung, um die PDFs im Browser anzeigen zu koennen. Versuche es erneut.")
+            document.getElementById("switch_pdf_newtab_block").style.visibility = "hidden"
           }
         }
-      );
+      )
     } else {
       //disable "pdf in new tab" setting since it doesn't make any sense without inline pdf
-      chrome.storage.local.set({ pdfInNewTab: false });
-      document.getElementById("switch_pdf_newtab").checked = false;
-      chrome.runtime.sendMessage({ cmd: "toggle_pdf_inline_setting", enabled: false });
+      chrome.storage.local.set({ pdfInNewTab: false })
+      document.getElementById("switch_pdf_newtab").checked = false
+      chrome.runtime.sendMessage({ cmd: "toggle_pdf_inline_setting", enabled: false })
     }
-  });
+  })
 
   document.getElementById("switch_pdf_newtab").addEventListener("click", function () {
-    chrome.storage.local.set({ pdfInNewTab: this.checked }, function () { });
-  });
+    chrome.storage.local.set({ pdfInNewTab: this.checked }, function () { })
+  })
 
   //set all switches and elements
   displayEnabled()
@@ -442,58 +442,58 @@ window.onload = async function () {
     }
     //update saved clicks  
     //see if any params are available
-    if (result.openSettingsPageParam === "auto_login_settings") { setTimeout(function () { this.document.getElementById("auto_login_settings").click(); }, 200); }
-    else if (result.openSettingsPageParam === "time_settings") { 
-      setTimeout(function () { this.document.getElementById("time_settings").click(); }, 200);
+    if (result.openSettingsPageParam === "auto_login_settings") { setTimeout(function () { this.document.getElementById("auto_login_settings").click() }, 200) }
+    else if (result.openSettingsPageParam === "time_settings") {
+      setTimeout(function () { this.document.getElementById("time_settings").click() }, 200)
       setTimeout(function () {
         window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
-      }, 500); 
+      }, 500)
     }
-    else if (result.openSettingsPageParam === "mailFetchSettings") { setTimeout(function () { this.document.getElementById("owa_mail_settings").click(); }, 200); }
-    else if (result.openSettingsPageParam === "opalCustomize") { setTimeout(function () { this.document.getElementById("opal_modifications").click(); }, 200); }    
+    else if (result.openSettingsPageParam === "mailFetchSettings") { setTimeout(function () { this.document.getElementById("owa_mail_settings").click() }, 200) }
+    else if (result.openSettingsPageParam === "opalCustomize") { setTimeout(function () { this.document.getElementById("opal_modifications").click() }, 200) }
     else if (result.openSettingsPageParam === "rocket_icons_settings") {
       setTimeout(function () {
-        this.document.getElementById("rocket_icons").click();
-      }, 200);
+        this.document.getElementById("rocket_icons").click()
+      }, 200)
       setTimeout(function () {
         window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
-      }, 500);
+      }, 500)
     }
-  //else if (result.gotInteractionOnHostPermissionExtension1) { document.getElementsByTagName("button")[0].click() }
+    //else if (result.gotInteractionOnHostPermissionExtension1) { document.getElementsByTagName("button")[0].click() }
 
 
-    
-if (result.saved_click_counter === undefined) { result.saved_click_counter = 0 }
+
+    if (result.saved_click_counter === undefined) { result.saved_click_counter = 0 }
     this.document.getElementById("settings_comment").innerHTML = "Bereits " + clicksToTimeNoIcon(result.saved_click_counter)
     chrome.storage.local.set({ openSettingsPageParam: false }, function () { })
   })
 
   //prep accordion
-  let acc = document.getElementsByClassName("accordion");
-  let i;
+  let acc = document.getElementsByClassName("accordion")
+  let i
   for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
       //--only open one accordions section at a time
-      let acc = document.getElementsByClassName("accordion");
-      let i;
+      let acc = document.getElementsByClassName("accordion")
+      let i
       for (i = 0; i < acc.length; i++) {
         if (acc[i] === this) continue //skip actual clicked element
         let pan = acc[i].nextElementSibling
         if (pan.style.maxHeight) {
-          pan.style.maxHeight = null;
+          pan.style.maxHeight = null
           acc[i].classList.toggle("active")
         }
       }
       //--
       //open clicked accordion section and set active
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
+      this.classList.toggle("active")
+      var panel = this.nextElementSibling
       if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
+        panel.style.maxHeight = null
       } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
+        panel.style.maxHeight = panel.scrollHeight + "px"
       }
-    });
+    })
   }
 
 
@@ -503,7 +503,7 @@ if (result.saved_click_counter === undefined) { result.saved_click_counter = 0 }
     if (!result.gotInteractionOnHostPermissionExtension1) {
       let hpDiv = document.getElementById("addition_host_permissions")
       hpDiv.innerHTML = '<p>TUfast braucht eine zus&auml;tzliche Berechtigung, damit alle Online-Portale der TU Dresden unterst&uuml;tz werden.<br>Dr&uuml;cke jetzt "Akzeptieren" um TUfast f&uuml;r alle Online-Portale zu verwenden.</p> <button class="button-deny" id="refuseDomains">Ablehnen</button><button id="acceptDomains" style="margin-left:30px;" class="button-accept">Akzeptieren</button>'
-      await new Promise(r => setTimeout(r, 500));
+      await new Promise(r => setTimeout(r, 500))
       document.getElementById("refuseDomains").addEventListener('click', denyHostPermissionS) //innerHTML is not async. However, it takes time to render, so lets wait 500ms
       document.getElementById("acceptDomains").addEventListener('click', requestHostPermissionS)
     }
