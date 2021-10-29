5e432f79aaecb451493968f6825454830530d45c
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-10-29 15:46:08 +0200

    Refactor forwarding scripts

--- src/contentScripts/fwd_opalError.js
+++ src/contentScripts/fwd_opalError.js
@@ -1,7 +1,7 @@
-chrome.storage.local.get(['fwdEnabled'], function (result) {
+chrome.storage.local.get(['fwdEnabled'], async (result) => {
   if (result.fwdEnabled) {
     console.log('fwd to opal from opal error')
-    chrome.runtime.sendMessage({ cmd: 'save_clicks', click_count: 1 })
+    await new Promise((resolve) => chrome.runtime.sendMessage({ cmd: 'save_clicks', click_count: 1 }, resolve))
     // click on forward to opal, which should be first element in list
     if (document.querySelectorAll('li>a')[0].text.includes('OPAL')) {
       document.querySelectorAll('li>a')[0].click()
