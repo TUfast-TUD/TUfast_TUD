2cdf1e177170cdfbb123e94725f64ac6bb0042e2
Lucas Fugmann <lucas@fugi.dev>
2021-11-08 01:08:30 +0100

    add basic snowpack configuration

---
+++ snowpack.config.js
@@ -0,0 +1,21 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    'src': { url: '/' },
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
