9acc72b924a918c14ac873c99d1b743ab50cf1dc
Noxdor <lennart@guderian.de>
2021-11-18 13:20:53 +0100

    running example

--- src/freshContent/settings/Settings.vue
+++ src/freshContent/settings/Settings.vue
@@ -1,16 +1,35 @@
 <template>
-    <h1>{{ x }}</h1>
-    <button v-if="toggle" @click="toggle = !toggle">Toggle Text</button>
+    <div>
+        <h1 v-if="toggle" class="message">{{ x }} From TUFast</h1>
+        <button class="pos-me test" @click="toggle = !toggle">Toggle Text</button>
+    </div>
 </template>
 
-<script lang="ts" setup>
-import { ref } from '@vue/reactivity'
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
 
-const x = ref<string>("Hello World!")
-const toggle = ref<boolean>(true)
+export default defineComponent({
+    setup() {
+        const x = ref<string>("Hello World!")
+        const toggle = ref<boolean>(true)
+        console.log(x.value)
+
+        return { x, toggle }
+    }
+})
+
 </script>
 
 <style lang="sass" scoped>
 .message
-    font-size: 10rem
+    font-size: 5rem
+
+.pos-me
+    position: fixed
+    left: 10px
+    top: 45vh
+    font-size: 3rem
+
+.test
+    color: red
 </style>
