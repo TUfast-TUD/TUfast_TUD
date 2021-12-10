f5f30f389779baa64fb486f5ed9a7b1bf3ac1748
Noxdor <lennart@guderian.de>
2021-12-10 14:41:13 +0100

    implemented statistics functionality

--- src/freshContent/settings/components/Statistics.vue
+++ src/freshContent/settings/components/Statistics.vue
@@ -8,25 +8,36 @@
         </div>
         <div class="statistics__minutes">
             <ph-timer />
-            <span class="txt-bold">2min</span>
-            <span>15s</span>
+            <span class="txt-bold">{{ getMinutes(counter) }}min</span>
+            <span>{{ getSeconds(counter) }}s</span>
         </div>
     </div>
 </template>
 
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, Ref } from 'vue'
 
 export default defineComponent({
     setup() {
 
-        const counter = ref(null)
+        const counter = ref<number>(0)
         chrome.storage.local.get(["saved_click_counter"], (clicks) => counter.value = clicks.saved_click_counter)
 
+        const getMinutes = (clicks : number) => {
+            const clickValue = clicks * 3
+            return Math.floor(clicks / 60)
+        }
+
+        const getSeconds = (clicks : number) => {
+            const clickValue = clicks * 3
+            return clicks % 60
+        }
 
         return {
             counter,
+            getMinutes,
+            getSeconds,
         }
         
     },
