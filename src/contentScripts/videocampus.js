e78faed1709cfd23ae8179651c0874811817d578
Daniel Kluge <daniel.kluge.98@gmx.de>
2021-12-20 00:01:18 +0100

    Add videocampus autologin

---
+++ src/contentScripts/videocampus.js
@@ -0,0 +1,29 @@
+function loginVideoCampus () {
+  if (document.querySelector('#login .loginOptions .form-control[name="entityID"]')) {
+    // The login form manipulation has to be first else we will always click on "login"
+    document.querySelector('#login .loginOptions .form-control[name="entityID"]').value = 'https://idp.tu-dresden.de/idp/shibboleth'
+    document.querySelector('#login .loginOptions input[type="submit"]').click()
+  } else if (document.querySelector('.nav-link[href="/login"]')) {
+    document.querySelector('.nav-link[href="/login"]').click()
+    chrome.runtime.sendMessage({ cmd: 'save_clicks', click_count: 1 })
+  } else if (document.querySelector('.dropdown-item[href="/logout"]')) {
+    // abmelden button
+    document.querySelector('.dropdown-item[href="/logout"]').addEventListener('click', () => {
+      const date = new Date()
+      date.setMinutes(date.getMinutes() + 2)
+      document.cookie = `vcLoggedOut; expires=${date.toUTCString()}; path=/; domain=.videocampus.sachsen.de; secure`
+    })
+  }
+
+  console.log('Auto Login to videocampus.')
+}
+
+chrome.storage.local.get(['isEnabled'], (result) => {
+  if (result.isEnabled && !document.cookie.includes('vcLoggedOut')) {
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', loginVideoCampus)
+    } else {
+      loginVideoCampus()
+    }
+  }
+})
