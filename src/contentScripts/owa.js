6bfae3374bb7545c067ba20e5d02a83f919734ad
Oli <olli.haus97@gmail.com>
2022-02-24 08:21:20 +0100

    Update owa.js

--- src/contentScripts/owa.js
+++ src/contentScripts/owa.js
@@ -41,7 +41,7 @@ function loginOWA (loggedOutOwa) {
         chrome.runtime.sendMessage({ cmd: 'show_ok_badge', timeout: 2000 })
         chrome.runtime.sendMessage({ cmd: 'save_clicks', click_count: 1 })
         chrome.runtime.sendMessage({ cmd: 'perform_login' })
-        document.getElementById('username').value = (result.asdf) + '@msx.tu-dresden.de'
+        document.getElementById('username').value = (result.asdf)
         document.getElementById('password').value = (result.fdsa)
         document.getElementsByClassName('signinbutton')[0].click()
         chrome.runtime.sendMessage({ cmd: 'logged_out', portal: 'loggedOutOwa' })
