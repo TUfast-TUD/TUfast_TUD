187e4f4dee592b7e2f4c70c9b188f6b49f26885d
Daniel Kluge <daniel-git@c0ntroller.de>
2022-05-24 14:42:36 +0200

    Migrate SLUB

---
+++ src/contentScripts/login/slub.ts
@@ -0,0 +1,49 @@
+// Although we can't use the ESM import statements in content scripts we can import types.
+import type { CookieSettings, UserData, Login, LoginFields } from './common'
+
+// "Quicksettings"
+const platform = 'slub'
+const cookieSettings: CookieSettings = {
+  portalName: 'slub',
+  domain: 'slub-dresden.de'
+};
+
+(async () => {
+  const common = await import(chrome.runtime.getURL('contentScripts/login/common.js'))
+
+  // For better syntax highlighting import the "Login" type from the common module and change it to "common.Login" when you're done.
+  class SlubLogin extends common.Login {
+    constructor () {
+      super(platform, cookieSettings)
+    }
+
+    async additionalFunctionsPreCheck (): Promise<void> { }
+
+    async additionalFunctionsPostCheck (): Promise<void> {}
+
+    async findCredentialsError(): Promise<boolean | HTMLElement | Element> {
+        return document.getElementsByClassName('form-error')[0]
+    }
+
+    async loginFieldsAvailable (): Promise<boolean | LoginFields> {
+      return {
+        usernameField: document.getElementById('username') as HTMLInputElement,
+        passwordField: document.getElementById('password') as HTMLInputElement,
+        submitButton: document.querySelector('input[type="submit"]') as HTMLInputElement
+      }
+    }
+
+    async findLogoutButtons (): Promise<HTMLElement[] | NodeList> {
+      return document.querySelectorAll('a[href^="https://www.slub-dresden.de/Shibboleth.sso/Logout"]')
+    }
+
+    async login (userData: UserData, loginFields?: LoginFields): Promise<void> {
+      if (!loginFields) return
+      loginFields.usernameField.value = userData.user
+      loginFields.passwordField.value = userData.pass
+      loginFields.submitButton.click()
+    }
+  }
+
+  await (new SlubLogin()).start()
+})()
