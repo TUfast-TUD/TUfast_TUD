cfd87a62dcf755021252ce55703e0d034b39ead5
Daniel Kluge <daniel-git@c0ntroller.de>
2022-05-24 15:33:19 +0200

    Migrate lsk

---
+++ src/contentScripts/login/lskonline.ts
@@ -0,0 +1,57 @@
+// Although we can't use the ESM import statements in content scripts we can import types.
+import type { CookieSettings, UserData, Login, LoginFields } from './common'
+
+// "Quicksettings"
+const platform = 'zih'
+const cookieSettings: CookieSettings = {
+  portalName: 'lskonline',
+  domain: 'lskonline.tu-dresden.de'
+};
+
+(async () => {
+  const common = await import(chrome.runtime.getURL('contentScripts/login/common.js'))
+
+  // For better syntax highlighting import the "Login" type from the common module and change it to "common.Login" when you're done.
+  class LSKLogin extends common.Login {
+    constructor() {
+      super(platform, cookieSettings)
+    }
+
+    async additionalFunctionsPreCheck(): Promise<void> { }
+
+    async additionalFunctionsPostCheck(): Promise<void> {
+      this.clickLogin()
+    }
+
+    clickLogin() {
+      const link = Array.from(document.getElementsByTagName('a')).find(link => link?.innerText === "Login")
+      if (link?.id.includes('selected')) return
+      else link?.click()
+    }
+
+    // We don't need this check for wrong credentials here as it's a nother page where "Login" is selected
+    // but no input fields are available.
+
+    async loginFieldsAvailable(): Promise<boolean | LoginFields> {
+      return {
+        usernameField: document.querySelector('input[name="j_username"]') as HTMLInputElement,
+        passwordField: document.querySelector('input[name="j_password"]') as HTMLInputElement,
+        submitButton: document.querySelector('input[type="submit"]') as HTMLInputElement
+      }
+    }
+
+    async findLogoutButtons(): Promise<HTMLElement[]|NodeList> {
+      // There should only be one button but let's be safe
+      return Array.from(document.getElementsByTagName('a')).filter(link => link?.innerText === "Logout")
+    }
+
+    async login(userData: UserData, loginFields?: LoginFields): Promise<void> {
+      if (!loginFields) return
+      loginFields.usernameField.value = userData.user
+      loginFields.passwordField.value = userData.pass
+      loginFields.submitButton.click()
+    }
+  }
+
+  await (new LSKLogin()).start()
+})()
