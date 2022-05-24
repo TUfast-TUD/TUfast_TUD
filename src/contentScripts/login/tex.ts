4fa2524399bdf06b79aa4e14fab5211926f36503
Daniel Kluge <daniel-git@c0ntroller.de>
2022-05-24 14:30:37 +0200

    Migrate tex

---
+++ src/contentScripts/login/tex.ts
@@ -0,0 +1,44 @@
+// Although we can't use the ESM import statements in content scripts we can import types.
+import type { CookieSettings, UserData, Login, LoginFields } from './common'
+
+// "Quicksettings"
+const platform = 'zih'
+const cookieSettings: CookieSettings = {
+  portalName: 'tex',
+  domain: 'tex.zih.tu-dresden.de',
+  usesIdp: true
+};
+
+(async () => {
+  const common = await import(chrome.runtime.getURL('contentScripts/login/common.js'))
+
+  // For better syntax highlighting import the "Login" type from the common module and change it to "common.Login" when you're done.
+  class TexLogin extends common.Login {
+    constructor() {
+      super(platform, cookieSettings)
+    }
+
+    async additionalFunctionsPreCheck(): Promise<void> { }
+
+    async additionalFunctionsPostCheck(): Promise<void> {
+      this.clickLogin()
+    }
+
+    clickLogin() {
+      (document.querySelector('a[href="/saml/login/go"]') as HTMLAnchorElement | null)?.click()
+    }
+
+    async loginFieldsAvailable(): Promise<boolean | LoginFields> {
+      return false
+    }
+
+    async findLogoutButtons(): Promise<HTMLElement[]> {
+      return [document.querySelector('form[action="/logout"] button')]
+    }
+
+    async login(_userData: UserData, _loginFields?: LoginFields): Promise<void> { }
+
+  }
+
+  await (new TexLogin()).start()
+})()
