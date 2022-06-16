7a9f8ffdb8bd0a6d82658aa43df912fced75b9de
Noxdor <lennart@guderian.de>
2022-06-16 14:06:26 +0200

    Changes color theme of the hisqis table and adds color helpers.
    This commit solves the requested color change by @C0ntroller.
    
    It also reimplements the color helpers, which were there in the
    
    old vue version. It also changes the usage of deprecated bgColor
    
    into a call to window.getComputedStyle etc.

--- src/contentScripts/other/hisqis/newTable.ts
+++ src/contentScripts/other/hisqis/newTable.ts
@@ -17,18 +17,53 @@ import { DataTable } from 'simple-datatables'
   newTable.id = 'gradeTable'
   newTable.style.display = 'none' // Hide by default so there's less flickering
 
-  // Caption for our new table
-  const newTableCaption = document.createElement('caption')
-  newTableCaption.innerText = 'Deine Notenübersicht'
+  // Header for our new table
+  const caption = document.createElement('caption')
+  const tableHeader = document.createElement('div')
+  tableHeader.classList.add("table-header")
+  caption.append(tableHeader)
+
+  // title element
+  const title = document.createElement('h3')
+  title.classList.add("table-header__title")
+  title.innerText = 'Deine Notenübersicht'
+
+  // flex div to display small color helpers
+  const colorHelpers = document.createElement('div')
+  colorHelpers.classList.add("table-header__helpers")
+
+  // create small color helpers
+  for (const [i, descriptor] of 
+  ["Modul", "Bestandene Prüfung", "Verhauene Prüfung"].entries()) {
+    const colorHelper = document.createElement('div')
+    colorHelper.classList.add("table-header__helper")
+    colorHelper.classList.add(`table-header__helper--${i}`)
+
+    const color = document.createElement('div')
+    color.classList.add("table-header__color")
+    color.classList.add(`table-header__color--${i}`)
+    colorHelper.append(color)
+
+    const colorText = document.createElement('div')
+    colorText.classList.add("table-header__color-text")
+    colorText.classList.add(`table-header__color-text--${i}`)
+    colorText.innerText = descriptor
+    colorHelper.append(colorText)
+
+    colorHelpers.append(colorHelper)
+  }
+
+  tableHeader.append(title, colorHelpers)
+
 
   // Header for the new table
   const newTableHead = document.createElement('thead')
   const newTableHeadRow = document.createElement('tr')
   for (const th of oldTableRows[1].getElementsByTagName('th')) {
     const newTh = document.createElement('th')
-    newTh.style.textAlign = th.align
-    newTh.style.width = th.width
-    newTh.innerHTML = th.innerHTML
+    newTh.style.textAlign = th.style.textAlign
+    newTh.style.width = th.style.width
+    newTh.innerText = th.innerText.trim()
     newTableHeadRow.append(newTh)
   }
   newTableHead.appendChild(newTableHeadRow)
@@ -43,17 +78,19 @@ import { DataTable } from 'simple-datatables'
     const newRow = document.createElement('tr')
     for (const cell of cells) {
       const newCell = document.createElement('td')
-      newCell.style.textAlign = cell.align
-      // newCell.style.width = cell.width
+      newCell.style.textAlign = cell.style.textAlign
+      newCell.style.width = cell.style.width
       newCell.innerText = cell.innerText
       newRow.appendChild(newCell)
     }
 
+    const computedStyleCell = window.getComputedStyle(cells[0])
+    const backgroundColorCell = computedStyleCell.getPropertyValue("background-color")
     switch (true) {
-      case cells[0].bgColor === '#ADADAD':
+      case backgroundColorCell === "rgb(173, 173, 173)":
         newRow.className = 'meta'
         break
-      case cells[0].bgColor === '#DDDDDD':
+      case backgroundColorCell === 'rgb(221, 221, 221)':
         newRow.className = 'module'
         break
       case Number.parseFloat(cells[3].textContent?.trim().replace(',', '.') || '') === 5:
@@ -66,7 +103,7 @@ import { DataTable } from 'simple-datatables'
   }
 
   // Append everything together
-  newTable.append(newTableCaption, newTableHead, newTableBody)
+  newTable.append(caption, newTableHead, newTableBody)
 
   /* let { hisqisPimpedTable } = { hisqisPimpedTable: true } // await new Promise<any>((resolve) => chrome.storage.local.get(['hisqisPimpedTable'], resolve))
   if (hisqisPimpedTable) oldTable.style.display = 'none'
@@ -74,8 +111,7 @@ import { DataTable } from 'simple-datatables'
 
   oldTable.parentNode?.insertBefore(newTable, oldTable)
 
-  // eslint-disable-next-line no-unused-vars
-  const dataTable = new DataTable(newTable, {
+  new DataTable(newTable, {
     sortable: true,
     searchable: false,
     paging: false,
