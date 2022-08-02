a64777c5020ee03911f5c13297fc0a7ed09c0c9d
Daniel Kluge <daniel-git@c0ntroller.de>
2022-08-02 14:45:57 +0200

    studies.json in popup

--- src/freshContent/popup/popup.js
+++ src/freshContent/popup/popup.js
@@ -1,107 +1,9 @@
+import studiengangConfig from "../studies.json"
+
 const shareHTML = '<div style=height:450px;width:510px;overflow:hidden><div class=the-middle style=white-space:nowrap;display:inline><div class=tufast_text><span class=tufasst_name>Hilf deinen Mitstudierenden</span></div><div class="tufast_text" style=position:relative;top:6px><img class="imgicon huge invert" src=../../assets/images/tufast48.png style=position:relative;top:-7px;left:0px><span class="tufasst_name huge" style=position:relative;top:-7px;left:3px>TUfast</span><span class=tufasst_name> &nbsp;zu entdecken</span></div><div class=grey><span class=tufasst_name>und <a class=grey_a id=rewards_link href=javascript:void(0)>sammle coole Raketen</a>!</span></div><div id=download-section><div>Teilen mit</div><div class=download-link><img class=imgicon src=../../assets/icons/gmail.png> <span class=browser_name><a href="mailto:?subject=Probiere%20mal%20TUfast!%20%F0%9F%9A%80&body=Hey%20%3A)%0A%0Akennst%20du%20schon%20TUfast%3F%0A%0ATUfast%20hilft%20beim%20t%C3%A4glichen%20Arbeiten%20mit%20den%20Online-Portalen%20der%20TU%20Dresden.%0ADamit%20spare%20ich%20viel%20Zeit%20und%20nervige%20Klicks.%0A%0ATUfast%20ist%20eine%20Erweiterung%20f%C3%BCr%20den%20Browser%20und%20wurde%20von%20Studenten%20entwickelt.%0AProbiere%20es%20jetzt%20auf%20www.tu-fast.de%20!%0A%0ALiebe%20Gr%C3%BC%C3%9Fe%C2%A0%F0%9F%96%90"target=_blank>E-Mail</a></span></div><div class=download-link><img class=imgicon src=../../assets/icons/wa2.png style=height:1.4em><span class=browser_name> <a href="https://api.whatsapp.com/send?text=Hey%2C%20kennst%20du%20schon%20TUfast%3F%20%F0%9F%9A%80%0A%0AMacht%20das%20arbeiten%20mit%20allen%20Online-Portalen%20der%20TU%20Dresden%20produktiver%20und%20hat%20mir%20schon%20viel%20Zeit%20und%20nervige%20Klicks%20gespart.%20Eine%20richtig%20n%C3%BCtzliche%20Browsererweiterung%20f%C3%BCr%20Studenten!%0A%0AProbiers%20gleich%20mal%20aus%20auf%20www.tu-fast.de%20%F0%9F%96%90"target=_blank>WhatsApp</a></span></div><div class=download-link><span class=browser_name>oder <a href=https://www.tu-fast.de target=_blank>www.tu-fast.de</a></span></div></div></div><div class=the-bottom><p>Gemacht mit 🖤 von Studenten | <a href=https://github.com/TUfast-TUD/TUfast_TUD target=_blank>GitHub</a> | <a href="mailto:frage@tu-fast.de?subject=Feedback%20TUfast"target=_blank>Kontakt</a></div></div>'
 const bananaHTML = '<a href="https://www.buymeacoffee.com/olihausdoerfer" target="_blank"style = "position: fixed; bottom: 68px; right: -75px; width:240px; height: auto;" > <img style="width: 160px;"src="https://img.buymeacoffee.com/button-api/?text=Buy me a Mate&emoji=🍾&slug=olihausdoerfer&button_colour=fbd54b&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"></a>'
 
-// this config is used to customize TUfast for a course of study
-// it overrides the default setting from popup.html
-// if you want to add an footer icon for your course of study, you need to add it to popup.html and set footer_icons_display property in this config
-const studiengangConfig = {
-  geowissenschaften: {
-    name: 'Geowissenschaften',
-    fsr_icon: '../../assets/icons/FSRIcons/fsr_geo.png',
-    fsr_link: 'https://tu-dresden.de/bu/umwelt/geo/fsr',
-    fsr_icon_2: '../../assets/icons/OPAL.png',
-    fsr_link_2: 'https://bildungsportal.sachsen.de/opal/auth/RepositoryEntry/15833497605',
-    fsr_icon_dashboard_style: '',
-    fsr_icon_dashboard_style_2: 'max-height: 20px; margin-top: 10px; padding-right:10px',
-    footer_icons_display: ['selma', 'opal', 'qis', 'matrix', 'msx', 'cloud', 'swdd', 'geoportal'],
-    footer_icons_links: {
-      swdd: 'https://www.studentenwerk-dresden.de/mensen/speiseplan/mensa-siedepunkt.html'
-    },
-    invert_icon_dark_theme: false
-  },
-  maschinenbau: {
-    name: 'Maschinenwesen',
-    fsr_icon: '../../assets/icons/FSRIcons/fsr_mw.png',
-    fsr_icon_dashboard_style: 'max-height: 32px;',
-    fsr_link: 'https://tu-dresden.de/ing/maschinenwesen/fsr',
-    footer_icons_display: ['selma', 'opal', 'qis', 'matrix', 'msx', 'cloud', 'swdd'],
-    footer_icons_links: {
-      swdd: 'https://www.studentenwerk-dresden.de/mensen/speiseplan/'
-    },
-    invert_icon_dark_theme: false
-  },
-  mathematik: {
-    name: 'Mathematik',
-    fsr_icon: '../../assets/icons/FSRIcons/fsr_mathe.png',
-    fsr_icon_dashboard_style: 'max-height: 32px;',
-    fsr_link: 'https://myfsr.de/dokuwiki/doku.php?id=start',
-    footer_icons_display: ['selma', 'opal', 'matrix', 'msx', 'cloud', 'gitlab', 'je', 'swdd'],
-    footer_icons_links: {
-      swdd: 'https://www.studentenwerk-dresden.de/mensen/speiseplan/'
-    },
-    invert_icon_dark_theme: false
-  },
-  medizin: {
-    name: 'Medizin',
-    fsr_icon: '../../assets/icons/FSRIcons/fsr_medi_small.png',
-    fsr_link: 'https://www.medforum-dresden.de/',
-    fsr_icon_dashboard_style: '',
-    footer_icons_display: ['selma', 'opal', 'moodle', 'eportal', 'msx', 'cloud', 'swdd'],
-    footer_icons_links: {
-      swdd: 'https://www.studentenwerk-dresden.de/mensen/speiseplan/mensologie.html'
-    },
-    invert_icon_dark_theme: false
-  },
-  psychologie: {
-    name: 'Psychologie',
-    fsr_icon: '../../assets/icons/FSRIcons/fsr_psy.png',
-    fsr_link: 'https://tu-dresden.de/mn/psychologie/fsrpsy',
-    fsr_icon_dashboard_style: '',
-    footer_icons_display: ['selma', 'opal', 'qis', 'matrix', 'msx', 'cloud', 'swdd', 'orsee'],
-    footer_icons_links: {
-      swdd: 'https://www.studentenwerk-dresden.de/mensen/speiseplan/mensa-siedepunkt.html'
-    },
-    invert_icon_dark_theme: false
-  },
-  wirtschaftswissenschaften: {
-    name: 'Wirtschaftswissenschaften',
-    fsr_icon: '../../assets/icons/FSRIcons/fsr_wiwi.png',
-    fsr_icon_dashboard_style: 'max-height: 32px;',
-    fsr_link: 'https://fsrwiwi.de',
-    footer_icons_display: ['selma', 'opal', 'qis', 'matrix', 'msx', 'cloud', 'swdd'],
-    footer_icons_links: {
-      swdd: 'https://www.studentenwerk-dresden.de/mensen/speiseplan/'
-    },
-    invert_icon_dark_theme: false
-  },
-  elektrotechnik: {
-    name: 'Elektrotechnik',
-    fsr_icon: '../../assets/icons/FSRIcons/FSR_ET.png',
-    fsr_icon_dashboard_style: 'max-height: 32px;',
-    fsr_link: 'https://fsret.de',
-    footer_icons_display: ['selma', 'opal', 'qis', 'matrix', 'msx', 'cloud', 'swdd'],
-    footer_icons_links: {
-      swdd: 'https://www.studentenwerk-dresden.de/mensen/speiseplan/'
-    },
-    invert_icon_dark_theme: true
-  },
-  general: {
-    name: 'Standardeinstellungen',
-    fsr_icon: '',
-    fsr_link: '',
-    footer_icons_display: ['selma', 'opal', 'qis', 'matrix', 'msx', 'cloud', 'je', 'swdd'],
-    footer_icons_links: {
-      swdd: 'https://www.studentenwerk-dresden.de/mensen/speiseplan/'
-    },
-    invert_icon_dark_theme: false
-  },
-  addStudiengang: {
-    name: '&#65291; Studiengang hinzufügen...'
-  }
-}
-
 const starRatingSettings = {
-
   // initial rating value
   rating: '0.0',
 
@@ -200,10 +102,10 @@ window.onload = async () => {
 
   // asign input search fct
   // onkeydown?
-  this.document.getElementById('searchListInput').onkeyup = listSearchFunction
+  document.getElementById('searchListInput').onkeyup = listSearchFunction
 
-  this.document.getElementById('settings').onclick = openSettings
-  this.document.getElementById('share').onclick = openShare
+  document.getElementById('settings').onclick = openSettings
+  document.getElementById('share').onclick = openShare
 
   await displayEnabled()
 
