e30fe3c25f8adbc7724708a4ce73a3e9507a539d
Noxdor <lennart@guderian.de>
2022-10-17 14:48:03 +0200

    Changes warning message for username input.

--- src/freshContent/settings/composables/logins.ts
+++ src/freshContent/settings/composables/logins.ts
@@ -1,34 +1,34 @@
-import type { Login } from '../types/Login'
+import type { Login } from "../types/Login";
 
 export const useLogins = () => ({
-  logins
-})
+  logins,
+});
 
 const logins: Login[] = [
   {
-    id: 'zih',
+    id: "zih",
     title:
-      'Werde in alle Online-Portale der TU Dresden automatisch angemeldet.',
-    name: 'Selma',
+      "Werde in alle Online-Portale der TU Dresden automatisch angemeldet.",
+    name: "Selma",
     state: false,
-    usernamePlaceholder: 'Nutzername (selma-Login)',
+    usernamePlaceholder: "Nutzername (selma-Login)",
     usernamePattern: /^(s{1}\d{7}|[a-z]{4}\d{3}[a-z])$/,
     usernameError:
-      "Normalerweise ohne @mailbox.tu-dresden.de in der Form 's3276763' oder 'luka075d'.",
-    passwordPlaceholder: 'Passwort (selma-Login)',
+      "Der Nutzername hat die Form 's3276763' oder 'luka075d'. Speichere deine aktuelle Eingabe nur, wenn du dir sicher bist.",
+    passwordPlaceholder: "Passwort (selma-Login)",
     passwordPattern: /.{5,}/,
-    passwordError: 'Das Passwort muss mindestens 5 Zeichen lang sein!'
+    passwordError: "Das Passwort muss mindestens 5 Zeichen lang sein!",
   },
   {
-    id: 'slub',
-    title: 'Werde automatisch auf der SLUB-Seite angemeldet.',
-    name: 'Slub',
+    id: "slub",
+    title: "Werde automatisch auf der SLUB-Seite angemeldet.",
+    name: "Slub",
     state: false,
-    usernamePlaceholder: 'Benutzernummer (SLUB-Login)',
+    usernamePlaceholder: "Benutzernummer (SLUB-Login)",
     usernamePattern: /^[0-9]{7}$/,
-    usernameError: 'die Nutzernummer findest du u.a. auf deiner SLUB-Karte',
-    passwordPlaceholder: 'Passwort (SLUB-Login)',
+    usernameError: "die Nutzernummer findest du u.a. auf deiner SLUB-Karte",
+    passwordPlaceholder: "Passwort (SLUB-Login)",
     passwordPattern: /.{5,}/,
-    passwordError: 'Das Passwort muss mindestens 5 Zeichen lang sein!'
-  }
-]
+    passwordError: "Das Passwort muss mindestens 5 Zeichen lang sein!",
+  },
+];
--- src/freshContent/settings/settingPages/AutoLogin.vue
+++ src/freshContent/settings/settingPages/AutoLogin.vue
@@ -1,8 +1,5 @@
 <template>
-  <LoginTabs
-    v-model="currentLogin"
-    :options="logins"
-  />
+  <LoginTabs v-model="currentLogin" :options="logins" />
   <h2>{{ currentLogin.title }}</h2>
   <h2
     :class="`state ${currentLogin.state ? 'state--active' : 'state--inactive'}`"
@@ -52,73 +49,73 @@
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, watchEffect } from 'vue'
+import { ref, defineComponent, watchEffect } from "vue";
 
 // components
-import Input from '../components/Input.vue'
-import Button from '../components/Button.vue'
-import LoginTabs from '../components/LoginTabs.vue'
+import Input from "../components/Input.vue";
+import Button from "../components/Button.vue";
+import LoginTabs from "../components/LoginTabs.vue";
 
 // composables
-import { useLogins } from '../composables/logins'
-import { useChrome } from '../composables/chrome'
-import { useUserData } from '../composables/user-data'
+import { useLogins } from "../composables/logins";
+import { useChrome } from "../composables/chrome";
+import { useUserData } from "../composables/user-data";
 
 export default defineComponent({
   components: {
     CustomInput: Input,
     CustomButton: Button,
-    LoginTabs
+    LoginTabs,
   },
-  setup () {
-    const { logins } = useLogins()
-    const { sendChromeRuntimeMessage } = useChrome()
-    const { saveUserData, deleteUserData } = useUserData()
+  setup() {
+    const { logins } = useLogins();
+    const { sendChromeRuntimeMessage } = useChrome();
+    const { saveUserData, deleteUserData } = useUserData();
 
-    const currentLogin = ref(logins[0])
+    const currentLogin = ref(logins[0]);
 
-    const username = ref('')
-    const password = ref('')
-    const usernameValid = ref(false)
-    const passwordValid = ref(false)
+    const username = ref("");
+    const password = ref("");
+    const usernameValid = ref(false);
+    const passwordValid = ref(false);
 
-    const autoLoginActive = ref(false)
+    const autoLoginActive = ref(false);
 
     // get state of login
     watchEffect(async () => {
       currentLogin.value.state = (await sendChromeRuntimeMessage({
-        cmd: 'check_user_data',
-        platform: currentLogin.value.id
-      })) as boolean
-    })
+        cmd: "check_user_data",
+        platform: currentLogin.value.id,
+      })) as boolean;
+    });
 
     const submitSave = async ($event: MouseEvent) => {
-      const target = $event.target as HTMLButtonElement
+      const target = $event.target as HTMLButtonElement;
 
-      if (target.disabled) return
+      if (target.disabled) return;
 
       // await this one to get back the new value in last line, otherwise could run too late
-      await saveUserData(username.value, password.value, currentLogin.value.id)
+      await saveUserData(username.value, password.value, currentLogin.value.id);
 
       // reset values
-      username.value = ''
-      password.value = ''
-      usernameValid.value = false
-      passwordValid.value = false
+      username.value = "";
+      password.value = "";
+      usernameValid.value = false;
+      passwordValid.value = false;
       currentLogin.value.state = (await sendChromeRuntimeMessage({
-        cmd: 'check_user_data',
-        platform: currentLogin.value.id
-      })) as boolean
-    }
+        cmd: "check_user_data",
+        platform: currentLogin.value.id,
+      })) as boolean;
+    };
 
     const submitDelete = async () => {
       // await this one to get back the new value in last line, otherwise could run too late
-      await deleteUserData(currentLogin.value.id)
+      await deleteUserData(currentLogin.value.id);
       currentLogin.value.state = (await sendChromeRuntimeMessage({
-        cmd: 'check_user_data',
-        platform: currentLogin.value.id
-      })) as boolean
-    }
+        cmd: "check_user_data",
+        platform: currentLogin.value.id,
+      })) as boolean;
+    };
 
     return {
       logins,
@@ -129,10 +126,10 @@ export default defineComponent({
       passwordValid,
       autoLoginActive,
       submitSave,
-      submitDelete
-    }
-  }
-})
+      submitDelete,
+    };
+  },
+});
 </script>
 
 <style lang="sass" scoped>
