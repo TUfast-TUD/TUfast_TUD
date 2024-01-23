df4cf1461d37e9e7af489e836231392e72054cb1
Oli <olli.haus97@gmail.com>
2024-01-23 16:13:00 +0100

    Merge Develop in Main (#140)
    * fix _execute_action for ff
    
    * Fix npm commands (#132)
    
    Thanks @A-K-O-R-A for noticing this bug and reporting it! Have a good day.
    
    * Feature/add hover texts (#133)
    
    * Add hover texts for the popup icons
    
    * Extend Informatik discord hover text
    
    * Correct spelling
    
    * Add OTP support (#131)
    
    * Add first classes for otp saving and generation
    
    * Typo
    
    * OTP saving complete
    
    * Better OTP saving
    
    * OTP filling
    
    * Add recovery codes to prompt
    
    * Eslint
    
    ---------
    
    Co-authored-by: Daniel Kluge <daniel-git@c0ntroller.de>
    Co-authored-by: Maf <65976562+A-K-O-R-A@users.noreply.github.com>
    Co-authored-by: Daniel <43993811+C0ntroller@users.noreply.github.com>

--- src/contentScripts/login/idp.ts
+++ src/contentScripts/login/idp.ts
@@ -22,6 +22,7 @@ const cookieSettings: CookieSettings = {
     async additionalFunctionsPostCheck (): Promise<void> {
       this.confirmData()
       this.outdatedRequest()
+      this.selectOTPType()
     }
 
     confirmData () {
@@ -29,8 +30,8 @@ const cookieSettings: CookieSettings = {
       if (!document.getElementById('generalConsentDiv')) return
 
       // Click the button
-      const button = document.querySelector('input[type="submit"][name="_eventId_proceed"]')
-      if (button) (button as HTMLInputElement).click()
+      const button = document.querySelector('input[type="submit"][name="_eventId_proceed"]') as HTMLInputElement | null
+      button?.click()
     }
 
     outdatedRequest () {
@@ -40,16 +41,40 @@ const cookieSettings: CookieSettings = {
       // We don't know where the user tried to login, so we can't jsut redirect to Opal/etc
     }
 
+    selectOTPType () {
+      if (!document.getElementById('fudis_selected_token_ids_input')) return
+
+      const button = document.querySelector('button[type="submit"][name="_eventId_proceed"]') as HTMLButtonElement | null
+      button?.click()
+    }
+
     async findCredentialsError (): Promise<boolean | HTMLElement | Element | null> {
       return document.querySelector('.content p font[color="red"]')
     }
 
     async loginFieldsAvailable (): Promise<boolean | LoginFields> {
-      return {
+      const fields: LoginFields = {
         usernameField: document.getElementById('username') as HTMLInputElement,
         passwordField: document.getElementById('password') as HTMLInputElement,
-        submitButton: document.querySelector('button[name="_eventId_proceed"][value="Login"]') as HTMLButtonElement
+        submitButton: document.querySelector('button[name="_eventId_proceed"][type="submit"]') as HTMLButtonElement
       }
+
+      const otpInput = document.getElementById('fudis_otp_input') as HTMLInputElement | null
+      if (otpInput) {
+        const indexesText = otpInput.parentElement?.parentElement?.querySelector('td:first-of-type')?.textContent?.trim()
+        //                            find number & number | remove whole match | to numbers | to zero based (first index is 0)
+        const indexes = indexesText?.match(/(\d+) & (\d+)/)?.slice(1, 3).map((x) => Number.parseInt(x, 10) - 1)
+
+        fields.otpSettings = {
+          input: otpInput,
+          submitButton: document.querySelector('button[name="_eventId_proceed"][type="submit"]') as HTMLButtonElement | null,
+          type: indexesText?.toLocaleLowerCase().includes('totp') ? 'totp' : 'iotp',
+          indexes: indexes && indexes.length > 0 ? indexes : undefined
+        }
+        console.log(fields)
+      }
+
+      return fields
     }
 
     async findLogoutButtons (): Promise<(HTMLElement|Element|null)[] | NodeList | null> {
