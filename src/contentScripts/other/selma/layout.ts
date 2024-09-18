fe6a264e4bb396d71e07f7fbc0c0caf3a34fab8c
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2024-09-18 17:22:59 +0200

    diagram

--- src/contentScripts/other/selma/layout.ts
+++ src/contentScripts/other/selma/layout.ts
@@ -1,4 +1,63 @@
 const currentView = document.location.pathname;
+// Regex for extracting Programm name and arguments from a popup Script
+// This is used to get the URL which would be opened in a popup
+const popupScriptsRegex =
+  /Message = dl_popUp\("\/scripts\/mgrqispi\.dll\?APPNAME=CampusNet&PRGNAME=(\w+)&ARGUMENTS=([^"]+)"/;
+
+function scriptToURL(script: string): string {
+  const matches = script.match(popupScriptsRegex)!;
+
+  const porgamName = matches.at(1)!;
+  const prgArguments = matches.at(2)!;
+
+  return `https://selma.tu-dresden.de/APP/${porgamName}/${prgArguments}`;
+}
+
+type GradeStat = {
+  grade: number;
+  count: number;
+};
+
+function maxGradeCount(values: GradeStat[]): number {
+  let max = 0;
+  for (const { grade, count } of values) {
+    if (count > max) max = count;
+  }
+  return max;
+}
+
+function totalGradeCount(values: GradeStat[]): number {
+  return values.map(({ grade, count }) => count).reduce((p, c) => p + c);
+}
+
+function calculateAverage(values: GradeStat[]): number {
+  return (
+    values.map(({ grade, count }) => grade * count).reduce((p, c) => p + c) /
+    totalGradeCount(values)
+  );
+}
+
+// Reduce the grade increments
+function pickGradeSubset(values: GradeStat[]): GradeStat[] {
+  const increments = [1, 1.3, 1.7, 2, 2.3, 2.7, 3, 3.3, 3.7, 4];
+
+  const newValues = increments.map((inc) => ({
+    grade: inc,
+    count: 0,
+  }));
+
+  let currentIncIndex = 0;
+  for (const { grade, count } of values) {
+    // Skip to next increment if we reached it's lower end
+    if (currentIncIndex !== increments.length - 1) {
+      const nextIncrement = increments[currentIncIndex + 1];
+      if (grade >= nextIncrement) currentIncIndex++;
+    }
+    newValues[currentIncIndex].count += count;
+  }
+
+  return newValues;
+}
 
 if (currentView.startsWith("/APP/EXAMRESULTS/")) {
   // Prüfungen > Ergebnisse
@@ -8,10 +67,88 @@ if (currentView.startsWith("/APP/EXAMRESULTS/")) {
   headRow.removeChild(headRow.children.item(3)!);
 
   const body = document.querySelector("tbody")!;
+  const promises: Promise<{ doc: Document; elm: Element }>[] = [];
   for (const row of body.children) {
     row.removeChild(row.children.item(3)!);
 
-    const firstCol = row.children.item(0)!;
-    firstCol.querySelector("div")!.style.color = "rgb(102, 102, 102)";
+    const lastCol = row.children.item(3)!;
+    const scriptContent = lastCol.children.item(1)!.innerHTML;
+
+    const url = scriptToURL(scriptContent);
+
+    promises.push(
+      fetch(url).then(async (s) => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(await s.text(), "text/html");
+
+        return { doc, elm: lastCol };
+      }),
+    );
   }
+
+  (async () => {
+    const gradeOverviews = await Promise.all(promises);
+
+    for (let i = 0; i < gradeOverviews.length; i++) {
+      const { doc, elm } = gradeOverviews[i];
+      const tableBody = doc.querySelector("tbody")!;
+      const values = [...tableBody.children]
+        .map((tr) => {
+          const gradeText = tr.children.item(0)!.textContent!.replace(",", ".");
+          const grade = parseFloat(gradeText);
+
+          const countText = tr.children.item(1)!.textContent!;
+          let count: number;
+          if (countText === "---") {
+            count = 0;
+          } else {
+            count = parseInt(countText);
+          }
+
+          return {
+            grade,
+            count,
+          };
+        })
+        .slice(0, -2); // Remove the 5.0 from all lists
+
+      // Avg
+      const avg = calculateAverage(values);
+      elm.innerHTML = `avg: ${avg.toFixed(2)}`;
+
+      // Drawing the Chart
+      const coarseValues = pickGradeSubset(values);
+
+      const width = 200;
+      const spacing = 0.1;
+      const barWidth = (width * (1 - spacing)) / coarseValues.length;
+      const height = 100;
+
+      let barsSvg = "";
+      const maxCount = maxGradeCount(coarseValues);
+      for (let x = 0; x < coarseValues.length; x++) {
+        const { grade, count } = coarseValues[x];
+        const barHeight = (count / maxCount) * height;
+
+        barsSvg += `
+        <rect
+          x="${x * barWidth * (1 + spacing)}" y="${height - barHeight}"
+          width="${barWidth}" height="${barHeight}"
+          rx="5" ry="5"
+        />
+        `;
+      }
+
+      elm.setAttribute("style", "vertical-align: middle;");
+      elm.innerHTML = `
+        <svg
+          viewBox="0 0 ${width} ${height}"
+          xmlns="http://www.w3.org/2000/svg">
+          ${barsSvg}
+        </svg>
+      `;
+
+      console.log(coarseValues);
+    }
+  })();
 }
