5918cac366e378fa877d279aee148a11b945ec64
AKORA <65976562+A-K-O-R-A@users.noreply.github.com>
2024-09-19 11:28:58 +0200

    reorganise code

--- src/contentScripts/other/selma/layout.ts
+++ src/contentScripts/other/selma/layout.ts
@@ -13,52 +13,6 @@ function scriptToURL(script: string): string {
   return `https://selma.tu-dresden.de/APP/${porgamName}/${prgArguments}`;
 }
 
-type GradeStat = {
-  grade: number;
-  count: number;
-};
-
-function maxGradeCount(values: GradeStat[]): number {
-  let max = 0;
-  for (const { grade, count } of values) {
-    if (count > max) max = count;
-  }
-  return max;
-}
-
-function totalGradeCount(values: GradeStat[]): number {
-  return values.map(({ grade, count }) => count).reduce((p, c) => p + c);
-}
-
-function calculateAverage(values: GradeStat[]): number {
-  return (
-    values.map(({ grade, count }) => grade * count).reduce((p, c) => p + c) /
-    totalGradeCount(values)
-  );
-}
-
-// Reduce the grade increments
-function pickGradeSubset(values: GradeStat[]): GradeStat[] {
-  const increments = [1, 1.3, 1.7, 2, 2.3, 2.7, 3, 3.3, 3.7, 4];
-
-  const newValues = increments.map((inc) => ({
-    grade: inc,
-    count: 0,
-  }));
-
-  let currentIncIndex = 0;
-  for (const { grade, count } of values) {
-    // Skip to next increment if we reached it's lower end
-    if (currentIncIndex !== increments.length - 1) {
-      const nextIncrement = increments[currentIncIndex + 1];
-      if (grade >= nextIncrement) currentIncIndex++;
-    }
-    newValues[currentIncIndex].count += count;
-  }
-
-  return newValues;
-}
-
 if (currentView.startsWith("/APP/EXAMRESULTS/")) {
   // Prüfungen > Ergebnisse
 
@@ -69,6 +23,9 @@ if (currentView.startsWith("/APP/EXAMRESULTS/")) {
   const body = document.querySelector("tbody")!;
   const promises: Promise<{ doc: Document; elm: Element }>[] = [];
   for (const row of body.children) {
+    // Remove useless inline styles which set the vertical alignment
+    for (const col of row.children) col.removeAttribute("style");
+
     row.removeChild(row.children.item(3)!);
 
     const lastCol = row.children.item(3)!;
@@ -90,82 +47,184 @@ if (currentView.startsWith("/APP/EXAMRESULTS/")) {
     const gradeOverviews = await Promise.all(promises);
 
     for (let i = 0; i < gradeOverviews.length; i++) {
+      // Parse the grade distributions
       const { doc, elm } = gradeOverviews[i];
       const tableBody = doc.querySelector("tbody")!;
-      const values = [...tableBody.children]
-        .map((tr) => {
-          const gradeText = tr.children.item(0)!.textContent!.replace(",", ".");
-          const grade = parseFloat(gradeText);
+      const values = [...tableBody.children].map((tr) => {
+        const gradeText = tr.children.item(0)!.textContent!.replace(",", ".");
+        const grade = parseFloat(gradeText);
 
-          const countText = tr.children.item(1)!.textContent!;
-          let count: number;
-          if (countText === "---") {
-            count = 0;
-          } else {
-            count = parseInt(countText);
-          }
+        const countText = tr.children.item(1)!.textContent!;
+        let count: number;
+        if (countText === "---") count = 0;
+        else count = parseInt(countText);
 
-          return {
-            grade,
-            count,
-          };
-        })
-        .slice(0, -2); // Remove the 5.0 from all lists
-
-      // Avg
-      const avg = calculateAverage(values);
-      elm.innerHTML = `avg: ${avg.toFixed(2)}`;
-
-      // Drawing the Chart
-      const coarseValues = pickGradeSubset(values);
-
-      const width = 200;
-      const spacing = 0.1;
-      const barWidth = (width * (1 - spacing)) / coarseValues.length;
-      const height = 100;
-
-      let barsSvg = "";
-      const maxCount = maxGradeCount(coarseValues);
-      for (let x = 0; x < coarseValues.length; x++) {
-        const { grade, count } = coarseValues[x];
-        const barHeight = (count / maxCount) * height;
-
-        barsSvg += `
-        <rect
-          x="${x * barWidth * (1 + spacing)}" y="${height - barHeight}"
-          width="${barWidth}" height="${barHeight}"
-          rx="5" ry="5"
-        />
-        `;
-      }
-
-      // Remove the inline vertical alignment
-      elm.setAttribute("style", "vertical-align: middle;");
+        return {
+          grade,
+          count,
+        };
+      });
+      // .slice(0, -2); // Remove the 5.0 from all lists
 
       // Present the bar chart
-      elm.innerHTML = `
-        <svg
-          viewBox="0 0 ${width} ${height}"
-          xmlns="http://www.w3.org/2000/svg">
-          ${barsSvg}
-        </svg>
-      `;
-
-      // Insert placeholder element for proper spacing
-      // elm.parentElement!.append(document.createElement("td"));
-
-      console.log(coarseValues);
+      const graphSVG = Graphing.createSVGGradeDistributionGraph(values);
+      elm.innerHTML = graphSVG;
     }
 
     // Remove the inline style that sets a width on the top right table cell
     const tableHeadRow = document.querySelector("thead>tr")!;
-
-    tableHeadRow.children.item(3)!.setAttribute("style", "");
-    // Add spacing element
-    /*
-    const spacer = document.createElement("th");
-    spacer.style.width = "2rem";
-    tableHeadRow.append(spacer);
-     */
+    tableHeadRow.children.item(3)!.removeAttribute("style");
   })();
+}
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Proabably a proper bundler config would be better
+
+*/
+
+namespace Graphing {
+  type GradeStat = {
+    grade: number;
+    count: number;
+  };
+
+  function maxGradeCount(values: GradeStat[]): number {
+    let max = 0;
+    for (const { count } of values) {
+      if (count > max) max = count;
+    }
+    return max;
+  }
+
+  function totalGradeCount(values: GradeStat[]): number {
+    return values.map(({ grade, count }) => count).reduce((p, c) => p + c);
+  }
+
+  function calculateAverage(values: GradeStat[]): number {
+    return (
+      values.map(({ grade, count }) => grade * count).reduce((p, c) => p + c) /
+      totalGradeCount(values)
+    );
+  }
+
+  // Reduce the grade increments
+  function pickGradeSubset(values: GradeStat[]): GradeStat[] {
+    const increments = [1, 1.3, 1.7, 2, 2.3, 2.7, 3, 3.3, 3.7, 4, 5];
+
+    const newValues = increments.map((inc) => ({
+      grade: inc,
+      count: 0,
+    }));
+
+    let currentIncIndex = 0;
+    for (const { grade, count } of values) {
+      // Skip to next increment if we reached it's lower end
+      if (currentIncIndex !== increments.length - 1) {
+        const nextIncrement = increments[currentIncIndex + 1];
+        if (grade >= nextIncrement) currentIncIndex++;
+      }
+      newValues[currentIncIndex].count += count;
+    }
+
+    return newValues;
+  }
+
+  export function createSVGGradeDistributionGraph(
+    values: GradeStat[],
+    width = 200,
+    height = 100,
+  ): string {
+    // Reduce the bar count / pick bigger intervals
+    const coarseValues = pickGradeSubset(values);
+
+    // Spacing in percent of bar width
+    const spacing = 0.1;
+    const barWidth = (width * (1 - spacing)) / coarseValues.length;
+
+    // Drawing the Chart
+    let barsSvg = "";
+    const maxCount = maxGradeCount(coarseValues);
+    for (let x = 0; x < coarseValues.length - 1; x++) {
+      const { count } = coarseValues[x];
+      const barHeight = (count / maxCount) * height;
+
+      barsSvg += `
+            <rect
+              class="passed"
+              x="${x * barWidth * (1 + spacing)}" y="${height - barHeight}"
+              width="${barWidth}" height="${barHeight}"
+              rx="5" ry="5"
+            />
+          `;
+    }
+
+    // Color the last rect for 5.0 / failed differently
+    {
+      const x = coarseValues.length - 1;
+      const { count } = coarseValues[x];
+      const barHeight = (count / maxCount) * height;
+
+      barsSvg += `
+            <rect
+              class="failed"
+              x="${x * barWidth * (1 + spacing)}" y="${height - barHeight}"
+              width="${barWidth}" height="${barHeight}"
+              rx="5" ry="5"
+            />
+        `;
+    }
+
+    return `
+      <svg
+        viewBox="0 0 ${width} ${height}"
+        xmlns="http://www.w3.org/2000/svg">
+        ${barsSvg}
+      </svg>
+    `;
+  }
 }
