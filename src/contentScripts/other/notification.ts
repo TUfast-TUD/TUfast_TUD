e9938b20a4cd76adc68064921d6839fbca511e91
thomsste <thomsen.steenkj@gmail.com>
2025-12-08 00:58:19 +0100

    design: notification style changes

--- src/contentScripts/other/notification.ts
+++ src/contentScripts/other/notification.ts
@@ -5,36 +5,44 @@ notificationContainer.classList.add('notifications')
 document.body.append(notificationContainer)
 
 export const notify = (msg: string) => {
-  // create a notification element, a close button and TUfast logo
+  // create notification element with all components
   const notification = document.createElement('div')
-  const closeButton = document.createElement('div')
-  const logo = document.createElement('img')
+  const closeButton = document.createElement('button')
+  const iconContainer = document.createElement('div')
+  const textParagraph = document.createElement('p')
 
   // configure notification
   notification.classList.add('notifications__notification')
-  notification.innerHTML = `<span>${msg}</span>`
 
   // configure close button
   closeButton.classList.add('notifications__close-button')
-  closeButton.innerText = 'X'
+  closeButton.innerHTML = '×'
   closeButton.onclick = () => {
     notification.classList.add('fade-out')
-    setTimeout(() => notification.remove(), 500)
+    setTimeout(() => notification.remove(), 400)
   }
 
-  // configure logo
-  logo.src = chrome.runtime.getURL('/assets/images/tufast48.png')
+  // configure icon with SVG checkmark
+  iconContainer.classList.add('notifications__icon')
+  iconContainer.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M5 12l5 5l10 -10" />
+    </svg>
+  `
 
-  // apend close icon & logo to notification and notification to container
-  notification.prepend(logo, closeButton)
+  // configure text
+  textParagraph.innerHTML = msg
+
+  // append elements to notification
+  notification.append(closeButton, iconContainer, textParagraph)
   notificationContainer.prepend(notification)
 
   // set timeout for notification to be removed automatically
   setTimeout(() => {
     notification.classList.add('fade-out')
-    // console.log("added fade-out")
-    setTimeout(() => notification.remove(), 500)
-  }, 50000)
+    setTimeout(() => notification.remove(), 400)
+  }, 10000)
 }
 
 export interface NotificationNamespace {
